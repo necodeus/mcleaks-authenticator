12427f376c8a39f9f6ac91eb8a4c45d9539971f5
Dawid <dawid.smulewicz@gmail.com>
2019-12-08 13:27:53 +0100

    Add files via upload

--- install.cmd
+++ install.cmd
@@ -6,20 +6,19 @@ set CURR_DIR=%~dp0
 :: and set your path:
 cd "C:\Program Files (x86)\Minecraft Launcher\runtime\jre-x64"
 
-:is_admin
-	net session >nul 2>&1
-	if %errorLevel% == 0 (
-		goto list
-	) else (
-		echo [101;93m - Administrative permissions required - [0m
-		goto quit
-	)
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	goto list
+) else (
+	echo [105;30m - Administrative permissions required - [0m
+	goto quit
+)
 
 :list
 	echo ###
-	echo # [101;93m 1 [0m Add certs
-	echo # [101;93m 2 [0m Remove certs
-	echo # [101;93m 3 [0m Show certs
+	echo # [103;30m 1 [0m Install hosts and certs
+	echo # [103;30m 2 [0m Remove hosts and certs
+	echo # [103;30m 3 [0m Show certs
 	echo ###
 	set /p choice="> "
 	if %choice% == 1 (
@@ -33,37 +32,48 @@ cd "C:\Program Files (x86)\Minecraft Launcher\runtime\jre-x64"
 	)
 
 :install
-	echo [101;93m - Java / authserver.mojang.com - [0m
-	bin\keytool -storepass "changeit" -import -alias authserver.mojang.com -file "%CURR_DIR%\authserver.mojang.com.crt" -keystore "lib\security\cacerts"
+	SET NEWLINE=^& echo.
+
+	FIND /C /I "authserver.mojang.com" %WINDIR%\system32\drivers\etc\hosts >nul 2>&1
+	IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^35.156.90.191 authserver.mojang.com>>%WINDIR%\System32\drivers\etc\hosts
 	
-	echo [101;93m - Java / sessionserver.mojang.com - [0m
-	bin\keytool -storepass "changeit" -import -alias sessionserver.mojang.com -file "%CURR_DIR%\sessionserver.mojang.com.crt" -keystore "lib\security\cacerts"
+	FIND /C /I "sessionserver.mojang.com" %WINDIR%\system32\drivers\etc\hosts >nul 2>&1
+	IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^35.156.90.191 sessionserver.mojang.com>>%WINDIR%\System32\drivers\etc\hosts
 	
-	echo [101;93m - Windows / Root / authserver.mojang.com - [0m
+	echo [102;30m - Java / authserver.mojang.com - [0m
+	bin\keytool -noprompt -storepass "changeit" -import -alias authserver.mojang.com -file "%CURR_DIR%\authserver.mojang.com.crt" -keystore "lib\security\cacerts"
+	
+	echo [102;30m - Java / sessionserver.mojang.com - [0m
+	bin\keytool -noprompt -storepass "changeit" -import -alias sessionserver.mojang.com -file "%CURR_DIR%\sessionserver.mojang.com.crt" -keystore "lib\security\cacerts"
+	
+	echo [102;30m - Windows / Root / authserver.mojang.com - [0m
 	certutil -addstore "Root" "%CURR_DIR%\authserver.mojang.com.crt"
 	
 	goto quit
 
 :uninstall
-	echo [101;93m - Java / authserver.mojang.com - [0m
+	findstr /v "authserver.mojang.com sessionserver.mojang.com" %WINDIR%\system32\drivers\etc\hosts > %temp%\testowy.txt
+	move /y %temp%\testowy.txt %WINDIR%\system32\drivers\etc\hosts >nul
+	
+	echo [101;30m - Java / authserver.mojang.com - [0m
 	bin\keytool -storepass "changeit" -delete -alias authserver.mojang.com -keystore "lib\security\cacerts"
 	
-	echo [101;93m - Java / sessionserver.mojang.com - [0m
+	echo [101;30m - Java / sessionserver.mojang.com - [0m
 	bin\keytool -storepass "changeit" -delete -alias sessionserver.mojang.com -keystore "lib\security\cacerts"
 	
-	echo [101;93m - Windows / Root / authserver.mojang.com - [0m
+	echo [101;30m - Windows / Root / authserver.mojang.com - [0m
 	certutil -delstore "Root" authserver.mojang.com
 	
 	goto quit
 
 :show
-	echo [101;93m - Java / authserver.mojang.com - [0m
+	echo [104;30m - Java / authserver.mojang.com - [0m
 	bin\keytool -list -storepass "changeit" -alias authserver.mojang.com -v -keystore "lib\security\cacerts"
 	
-	echo [101;93m - Java / sessionserver.mojang.com - [0m
+	echo [104;30m - Java / sessionserver.mojang.com - [0m
 	bin\keytool -list -storepass "changeit" -alias sessionserver.mojang.com -v -keystore "lib\security\cacerts"
 	
-	echo [101;93m - Windows / Root / authserver.mojang.com - [0m
+	echo [104;30m - Windows / Root / authserver.mojang.com - [0m
 	certutil -store "Root" authserver.mojang.com
 	
 	goto quit
